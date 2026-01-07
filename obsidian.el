0b682bc2fe78999c282b26a7142dcb0057a8bb21
reindert <info@codesensei.nl>
2026-01-07 21:44:18 +0100

    change dependency on emacs 27 -> 28

--- obsidian.el
+++ obsidian.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/licht1stein/obsidian.el
 ;; Keywords: obsidian, pkm, convenience
 ;; Version: 1.4.4
-;; Package-Requires: ((emacs "27.2") (f "0.2.0") (s "1.12.0") (dash "2.13") (markdown-mode "2.5") (elgrep "1.0.0") (yaml "0.5.1") (ht "2.3"))
+;; Package-Requires: ((emacs "28") (f "0.2.0") (s "1.12.0") (dash "2.13") (markdown-mode "2.5") (elgrep "1.0.0") (yaml "0.5.1") (ht "2.3"))
 ;; This file is NOT part of GNU Emacs.
 
 ;;; License:
